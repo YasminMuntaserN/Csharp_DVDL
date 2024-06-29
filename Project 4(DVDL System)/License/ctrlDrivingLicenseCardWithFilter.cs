a832bd6f569a6f57d77a7c97c39632192111ad69
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-06-29 12:50:55 +0300

    feat: add ctrl Driving License card with filter  - Through this User Control, I added a filter to search for a driver’s license using its ID   - filled in the information for this license through the User Control that we created previously.

---
+++ Project 4(DVDL System)/License/ctrlDrivingLicenseCardWithFilter.cs
@@ -0,0 +1,82 @@
+﻿using DVDL_BusinessLayer_;
+using GumroadLicensing;
+using Project_4_DVDL_System_.Global_Classes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_4_DVDL_System_.License
+{
+    public partial class ctrlDrivingLicenseCardWithFilter : UserControl
+    {
+        private int? _LicenceID = null;
+
+        private clsLicense _License = null;
+
+        public int? LicenseID => _LicenceID;
+        public clsLicense License => ctrlDriverLicenseInfo1.License;
+
+        public ctrlDrivingLicenseCardWithFilter()
+        {
+            InitializeComponent();
+        }
+
+        public Action<int?> LicenseIDBack;
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            if (!this.ValidateChildren())
+            {
+                //Here we dont continue becuase the form is not valid
+                MessageBox.Show("Some fileds are not valide!, put the mouse over the red icon(s) to see the erro", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtLicenseID.Focus();
+                return;
+
+            }
+
+            int LicenseID = Convert.ToInt32(txtLicenseID.Text);
+            LoadLicenseInfo(LicenseID);
+        }
+
+        public void LoadLicenseInfo(int LicenseID)
+        {
+            txtLicenseID.Text = LicenseID.ToString();
+            ctrlDriverLicenseInfo1.LoadLicenseInfo(LicenseID);
+            _LicenceID = ctrlDriverLicenseInfo1.LicenseID;
+            LicenseIDBack?.Invoke(_License?.LicenseID);
+        }
+
+        private void txtLicenseID_Validating(object sender, CancelEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtLicenseID.Text.Trim()))
+            {
+                e.Cancel = true;
+                errorProvider1.SetError(txtLicenseID, "This field is required!");
+            }
+            else
+            {
+                //e.Cancel = false;
+                errorProvider1.SetError(txtLicenseID, null);
+            }
+        }
+
+        private void txtLicenseID_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+
+
+            // Check if the pressed key is Enter (character code 13)
+            if (e.KeyChar == (char)13)
+            {
+
+                btnFind.PerformClick();
+            }
+        }
+    }
+}
